ded1f67478874d130afee4dc4b05906cac183952
ZiyaOzgul <ziya.d.ozgul@gmail.com>
2026-06-03 16:26:09 +0300

    fixes

--- plugins/withAndroidManifestFix.js
+++ plugins/withAndroidManifestFix.js
@@ -1,17 +1,15 @@
-import { ConfigPlugin, withAndroidManifest } from "@expo/config-plugins";
+const { withAndroidManifest } = require("@expo/config-plugins");
 
-const withAndroidManifestFix: ConfigPlugin = (config) => {
+const withAndroidManifestFix = (config) => {
   return withAndroidManifest(config, async (config) => {
     const application = config.modResults.manifest.application?.[0];
 
     if (application?.$) {
-      // Secure store'un kurallarını koru, Adapty'inkini ez
       application.$["android:dataExtractionRules"] =
         "@xml/secure_store_data_extraction_rules";
       application.$["android:fullBackupContent"] =
         "@xml/secure_store_backup_rules";
 
-      // Merger'a "bu attribute'u replace et, merge etme" de
       application.$["xmlns:tools"] = "http://schemas.android.com/tools";
       application.$["tools:replace"] =
         "android:dataExtractionRules,android:fullBackupContent";
@@ -21,4 +19,4 @@ const withAndroidManifestFix: ConfigPlugin = (config) => {
   });
 };
 
-export default withAndroidManifestFix;
+module.exports = withAndroidManifestFix;
